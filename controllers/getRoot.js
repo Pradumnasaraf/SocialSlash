6f7d32dd97369112d9d8e49d225970e5f91d5693
Pradumna Saraf <pradumnasaraf@gmail.com>
2023-02-10 18:47:07 +0530

    feat: refactor the API

---
+++ controllers/getRoot.js
@@ -0,0 +1,5 @@
+const getRoot = (req, res) => {
+  res.send("API is working fine. Let's hit some endpoints");
+};
+
+module.exports = getRoot;
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
 const app = express();
 
 app.use(require("./middleware/logger.js"));
 app.use(require("./routes/router.js"));
 app.use(require("./middleware/404.js"));
 
-const PORT = process.PORT || 3000;
+const PORT = process.env.PORT || 9002;
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);

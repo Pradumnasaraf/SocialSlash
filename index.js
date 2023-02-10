684c15be8e810820a1f210590122c3d1e6ae755d
Pradumna Saraf <pradumnasaraf@gmail.com>
2023-02-10 19:17:49 +0530

    feat: add config folder and env POST support

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
+const { PORT } = require("./config/config.js");
 
 app.use(require("./middleware/logger.js"));
 app.use(require("./routes/router.js"));
 app.use(require("./middleware/404.js"));
-
-const PORT = process.env.PORT || 9002;
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
